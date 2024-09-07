9e0d4f7be318b5aca405b5a8f3568c7a4855e14c
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-09-07 12:17:19 +1200

    Move all fields and methods shared by suspect controllers to the AbstractSuspectController class; enable sending messages by pressing ENTER.

--- src/main/java/nz/ac/auckland/se206/controllers/AbstractSuspectController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/AbstractSuspectController.java
@@ -1,17 +1,38 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import nz.ac.auckland.apiproxy.chat.openai.ChatCompletionRequest;
+import nz.ac.auckland.apiproxy.chat.openai.ChatCompletionResult;
+import nz.ac.auckland.apiproxy.chat.openai.ChatMessage;
+import nz.ac.auckland.apiproxy.chat.openai.Choice;
+import nz.ac.auckland.apiproxy.config.ApiProxyConfig;
+import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
 import nz.ac.auckland.se206.GameStateContext;
+import nz.ac.auckland.se206.prompts.PromptEngineering;
 import nz.ac.auckland.se206.speech.TextToSpeech;
 
 public abstract class AbstractSuspectController {
+
+  @FXML private TextArea txtaChat;
+  @FXML private TextField txtInput;
+  @FXML private Button btnSend;
 
   @FXML protected Button btnToSwitch;
   @FXML private Label lblProfession;
 
   protected String suspectId;
+  private ChatCompletionRequest chatCompletionRequest;
+  private String profession;
 
   protected static boolean isFirstTimeInit = true;
   protected static GameStateContext context;
@@ -23,7 +44,7 @@ public abstract class AbstractSuspectController {
   @FXML
   public abstract void switchSuspectScene();
 
-  public abstract void setProfession(String profession);
+  // public abstract void setProfession(String profession);
 
     /**
    * Initializes the suspect view. If it's the first time initialization, it will provide instructions
@@ -40,6 +61,120 @@ public abstract class AbstractSuspectController {
     }
     lblProfession.setText(context.getProfessionToGuess());
     setProfession(context.getProfession(suspectId));
+  }
+
+  
+  /**
+   * Handles the key pressed event.
+   *
+   * @param event the key event
+   */
+  @FXML
+  public void onKeyPressed(KeyEvent event) throws ApiProxyException {
+    // Sends the message in the text field if the user presses ENTER.
+    if (event.getCode().toString().equals("ENTER")) {
+      sendMessage();
+    }
+  }
+
+  /**
+   * Handles the key released event.
+   *
+   * @param event the key event
+   */
+  @FXML
+  public void onKeyReleased(KeyEvent event) {
+  }
+
+    /**
+   * Generates the system prompt based on the profession.
+   *
+   * @return the system prompt string
+   */
+  private String getSystemPrompt() {
+    Map<String, String> map = new HashMap<>();
+    map.put("profession", profession);
+    return PromptEngineering.getPrompt("chat.txt", map);
+  }
+
+  /**
+   * Sets the profession for the chat context and initializes the ChatCompletionRequest.
+   *
+   * @param profession the profession to set
+   */
+  public void setProfession(String profession) {
+    this.profession = profession;
+    try {
+      ApiProxyConfig config = ApiProxyConfig.readConfig();
+      chatCompletionRequest =
+          new ChatCompletionRequest(config)
+              .setN(1)
+              .setTemperature(0.2)
+              .setTopP(0.5)
+              .setMaxTokens(100);
+      runGpt(new ChatMessage("system", getSystemPrompt()));
+    } catch (ApiProxyException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Appends a chat message to the chat text area.
+   *
+   * @param msg the chat message to append
+   */
+  private void appendChatMessage(ChatMessage msg) {
+    txtaChat.appendText(msg.getRole() + ": " + msg.getContent() + "\n\n");
+  }
+
+  /**
+   * Runs the GPT model with a given chat message.
+   *
+   * @param msg the chat message to process
+   * @return the response chat message
+   * @throws ApiProxyException if there is an error communicating with the API proxy
+   */
+  private ChatMessage runGpt(ChatMessage msg) throws ApiProxyException {
+    System.out.println(msg.getContent());
+    chatCompletionRequest.addMessage(msg);
+    try {
+      ChatCompletionResult chatCompletionResult = chatCompletionRequest.execute();
+      Choice result = chatCompletionResult.getChoices().iterator().next();
+      chatCompletionRequest.addMessage(result.getChatMessage());
+      appendChatMessage(result.getChatMessage());
+      TextToSpeech.speak(result.getChatMessage().getContent());
+      return result.getChatMessage();
+    } catch (ApiProxyException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  /**
+   * Calls the sendMessage function to send a message to the GPT model.
+   *
+   * @param event the action event triggered by the send button
+   * @throws ApiProxyException if there is an error communicating with the API proxy
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void onSendMessage(ActionEvent event) throws ApiProxyException, IOException {
+    sendMessage();
+  }
+
+  /**
+   * Sends a message to the GPT model and runs the GPT model.
+   * @throws ApiProxyException
+   */
+  private void sendMessage() throws ApiProxyException {
+    String message = txtInput.getText().trim();
+    if (message.isEmpty()) {
+      return;
+    }
+    txtInput.clear();
+    ChatMessage msg = new ChatMessage("user", message);
+    appendChatMessage(msg);
+    runGpt(msg);
   }
   
 }
--- src/main/java/nz/ac/auckland/se206/controllers/Suspect2Controller.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Suspect2Controller.java
@@ -1,42 +1,19 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import nz.ac.auckland.apiproxy.chat.openai.ChatCompletionRequest;
-import nz.ac.auckland.apiproxy.chat.openai.ChatCompletionResult;
-import nz.ac.auckland.apiproxy.chat.openai.ChatMessage;
-import nz.ac.auckland.apiproxy.chat.openai.Choice;
-import nz.ac.auckland.apiproxy.config.ApiProxyConfig;
-import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.controllers.SceneManager.AppUi;
-import nz.ac.auckland.se206.prompts.PromptEngineering;
-import nz.ac.auckland.se206.speech.TextToSpeech;
 
 /**
  * Controller class for the suspect2 view. Handles user interactions within the suspect2 scene where the user can
  * chat with suspect 2 and guess their profession.
  */
 public class Suspect2Controller extends AbstractSuspectController {
-  @FXML private TextArea txtaChat;
-  @FXML private TextField txtInput;
-  @FXML private Button btnSend;
-
-  private ChatCompletionRequest chatCompletionRequest;
-  private String profession;
-
   public Suspect2Controller() {
     suspectId = "rectPerson2";
   }
-  
+
   @Override
   @FXML
   public void switchSuspectScene() {
@@ -45,108 +22,6 @@ public class Suspect2Controller extends AbstractSuspectController {
     } catch (IOException e) {
       e.printStackTrace();
     }
-  }
-
-  /**
-   * Handles the key pressed event.
-   *
-   * @param event the key event
-   */
-  @FXML
-  public void onKeyPressed(KeyEvent event) {
-    System.out.println("Key " + event.getCode() + " pressed");
-  }
-
-  /**
-   * Handles the key released event.
-   *
-   * @param event the key event
-   */
-  @FXML
-  public void onKeyReleased(KeyEvent event) {
-    System.out.println("Key " + event.getCode() + " released");
-  }
-
-    /**
-   * Generates the system prompt based on the profession.
-   *
-   * @return the system prompt string
-   */
-  private String getSystemPrompt() {
-    Map<String, String> map = new HashMap<>();
-    map.put("profession", profession);
-    return PromptEngineering.getPrompt("chat.txt", map);
-  }
-
-  /**
-   * Sets the profession for the chat context and initializes the ChatCompletionRequest.
-   *
-   * @param profession the profession to set
-   */
-  public void setProfession(String profession) {
-    this.profession = profession;
-    try {
-      ApiProxyConfig config = ApiProxyConfig.readConfig();
-      chatCompletionRequest =
-          new ChatCompletionRequest(config)
-              .setN(1)
-              .setTemperature(0.2)
-              .setTopP(0.5)
-              .setMaxTokens(100);
-      runGpt(new ChatMessage("system", getSystemPrompt()));
-    } catch (ApiProxyException e) {
-      e.printStackTrace();
-    }
-  }
-
-  /**
-   * Appends a chat message to the chat text area.
-   *
-   * @param msg the chat message to append
-   */
-  private void appendChatMessage(ChatMessage msg) {
-    txtaChat.appendText(msg.getRole() + ": " + msg.getContent() + "\n\n");
-  }
-
-  /**
-   * Runs the GPT model with a given chat message.
-   *
-   * @param msg the chat message to process
-   * @return the response chat message
-   * @throws ApiProxyException if there is an error communicating with the API proxy
-   */
-  private ChatMessage runGpt(ChatMessage msg) throws ApiProxyException {
-    chatCompletionRequest.addMessage(msg);
-    try {
-      ChatCompletionResult chatCompletionResult = chatCompletionRequest.execute();
-      Choice result = chatCompletionResult.getChoices().iterator().next();
-      chatCompletionRequest.addMessage(result.getChatMessage());
-      appendChatMessage(result.getChatMessage());
-      TextToSpeech.speak(result.getChatMessage().getContent());
-      return result.getChatMessage();
-    } catch (ApiProxyException e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
-
-  /**
-   * Sends a message to the GPT model.
-   *
-   * @param event the action event triggered by the send button
-   * @throws ApiProxyException if there is an error communicating with the API proxy
-   * @throws IOException if there is an I/O error
-   */
-  @FXML
-  private void onSendMessage(ActionEvent event) throws ApiProxyException, IOException {
-    String message = txtInput.getText().trim();
-    if (message.isEmpty()) {
-      return;
-    }
-    txtInput.clear();
-    ChatMessage msg = new ChatMessage("user", message);
-    appendChatMessage(msg);
-    runGpt(msg);
   }
 
 }
