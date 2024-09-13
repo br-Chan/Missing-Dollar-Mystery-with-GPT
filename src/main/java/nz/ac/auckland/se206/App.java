b2497fe78312bf7ab57beac9e5e9588eb9380fbd
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-09-13 21:45:26 +1200

    Create helper methods for adding all scenes to scene manager and showing the first scene, the latter of which has an input parameter for setting which scene is shown.

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -3,14 +3,11 @@ package nz.ac.auckland.se206;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import nz.ac.auckland.se206.SceneManager.AppUi;
-import nz.ac.auckland.se206.controllers.ChatController;
 import nz.ac.auckland.se206.speech.FreeTextToSpeech;
 
 /**
@@ -35,7 +32,6 @@ public class App extends Application {
     return scene;
   }
 
-
   public static Parent getRoomController() {
     return roomController;
   }
@@ -52,7 +48,7 @@ public class App extends Application {
 
   /**
    * Sets the root of the scene to the value of the specified AppUi in the scene manager.
-   * 
+   *
    * @param appUi the enum key for the hash map in the scene manager.
    * @throws IOException if the key is not found.
    */
@@ -73,43 +69,53 @@ public class App extends Application {
   }
 
   /**
-   * Opens the chat view and sets the profession in the chat controller.
+   * Adds all FXML UI roots to the scene manager.
    *
-   * @param event the mouse event that triggered the method
-   * @param profession the profession to set in the chat controller
    * @throws IOException if the FXML file is not found
    */
-  public static void openChat(MouseEvent event, String profession) throws IOException {
-    FXMLLoader loader = new FXMLLoader(App.class.getResource("/fxml/chat.fxml"));
-    Parent root = loader.load();
+  private void addAllUi() throws IOException {
+    SceneManager.addUi(AppUi.MENU, loadFxml("menu"));
+    SceneManager.addUi(AppUi.GAME, loadFxml("game"));
 
-    ChatController chatController = loader.getController();
-    chatController.setProfession(profession);
-
-    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    scene = new Scene(root);
-    stage.setScene(scene);
-    stage.show();
-  }
-
-  /**
-   * This method is invoked when the application starts. It loads and shows the "suspect1" scene.
-   *
-   * @param stage the primary stage of the application
-   * @throws IOException if the "src/main/resources/fxml/suspect1.fxml" file is not found
-   */
-  @Override
-  public void start(final Stage stage) throws IOException {
-    // Add FXMLs to the scene manager hash map.
+    SceneManager.addUi(AppUi.CRIME_SCENE, loadFxml("crimeScene"));
     SceneManager.addUi(AppUi.SUSPECT1, loadFxml("suspect1"));
     SceneManager.addUi(AppUi.SUSPECT2, loadFxml("suspect2"));
     SceneManager.addUi(AppUi.SUSPECT3, loadFxml("suspect3"));
 
-    Parent root = SceneManager.getUiRoot(AppUi.SUSPECT1);
+    SceneManager.addUi(AppUi.GUESS, loadFxml("guess"));
+    SceneManager.addUi(AppUi.RESULT, loadFxml("result"));
+  }
+
+  /**
+   * Sets the scene that is shown first to the UI root associated with the input AppUi value, then
+   * displays the stage to the user.
+   */
+  private void showFirstScene(final Stage stage, AppUi firstAppUi) {
+    Parent root = SceneManager.getUiRoot(firstAppUi);
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
     stage.setOnCloseRequest(event -> handleWindowClose(event));
+  }
+
+  /**
+   * This method is invoked when the application starts. It loads and shows the first scene.
+   *
+   * @param stage the primary stage of the application
+   * @throws IOException if the FXML file is not found
+   */
+  @Override
+  public void start(final Stage stage) throws IOException {
+    addAllUi();
+
+    /**
+     * NOTE TO DEVELOPERS: Change the value of firstAppUi to change which scene is shown first when
+     * loading the game during testing. firstAppUi should be Appui.MENU upon merging with main
+     * (ideally) and upon release (as a must).
+     */
+    AppUi firstAppUi = AppUi.MENU;
+
+    showFirstScene(stage, firstAppUi);
   }
 
   private void handleWindowClose(WindowEvent event) {
