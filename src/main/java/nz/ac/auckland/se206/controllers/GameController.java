eeea15417169347b8a1eb142defcba0bc89c884c
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-09-17 10:03:52 +1200

    feat: implement timer task to GameController to countdown, and to switch to guess scene when time elapses.

--- src/main/java/nz/ac/auckland/se206/controllers/GameController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/GameController.java
@@ -1,10 +1,15 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
@@ -21,6 +26,8 @@ import nz.ac.auckland.se206.SceneManager.AppUi;
  */
 public class GameController {
 
+  public static final int GAMETIME = 7;
+
   @FXML private Pane gameView;
 
   @FXML private Button suspect1Button;
@@ -28,13 +35,56 @@ public class GameController {
   @FXML private Button suspect3Button;
   @FXML private Button crimeSceneButton;
 
-  /**
-   * Initializes the game scene and sets the initial game view.
-   */
+  @FXML private Label timerLabel;
+
+  private Timer timer;
+  private int timeLeft;
+
+  /** Initializes the game scene and sets the initial game view. */
   @FXML
   public void initialize() {
     System.out.println("Initialising game scene...");
     setGameView(AppUi.SUSPECT1);
+    beginCountdown(GAMETIME);
+  }
+
+  public void beginCountdown(int startingTime) {
+    timeLeft = startingTime;
+    timer = new Timer();
+
+    TimerTask task =
+        new TimerTask() {
+          @Override
+          public void run() {
+            Platform.runLater(
+                () -> {
+                  // Decrement the time left and update the label.
+                  timeLeft--;
+                  int minutesLeft = timeLeft / 60;
+                  int secondsLeft = timeLeft - 60 * minutesLeft;
+                  String digitalTimeLeft = null;
+                  if (secondsLeft < 10) {
+                    digitalTimeLeft = "0" + minutesLeft + ":0" + secondsLeft;
+                  } else {
+                    digitalTimeLeft = "0" + minutesLeft + ":" + secondsLeft;
+                  }
+                  timerLabel.setText(digitalTimeLeft);
+                  System.out.println(digitalTimeLeft);
+
+                  // Stop the timer if 0 is reached and handle it.
+                  if (timeLeft <= 0) {
+                    timer.cancel();
+                    handleTimeUp(startingTime);
+                  }
+                });
+          }
+        };
+
+    timer.scheduleAtFixedRate(task, 1000, 1000);
+  }
+
+  private void handleTimeUp(int startingTime) {
+    App.getScene().setRoot(SceneManager.getUiRoot(AppUi.GUESS));
   }
 
   /**
@@ -60,6 +110,7 @@ public class GameController {
   private void setGameView(AppUi appUi) {
     gameView.getChildren().setAll(SceneManager.getUiRoot(appUi));
   }
+
   @FXML
   public void handleSuspect1ButtonClick(ActionEvent event) {
     setGameView(AppUi.SUSPECT1);
