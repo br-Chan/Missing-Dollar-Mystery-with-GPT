2dfe4d7422ce3532e94f6040ae2813255b1ede21
Perry Xie <perryxie.nz@gmail.com>
2024-09-17 19:24:10 +1200

    Add a selected icon on the current scene on the minimap.

--- src/main/java/nz/ac/auckland/se206/controllers/GameController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/GameController.java
@@ -1,15 +1,13 @@
 package nz.ac.auckland.se206.controllers;
 
-import java.util.Timer;
-import java.util.TimerTask;
-import javafx.application.Platform;
+import java.util.ArrayList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.AppTimer;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
@@ -34,18 +32,26 @@ public class GameController {
   @FXML private Button suspect3Button;
   @FXML private Button crimeSceneButton;
 
+  @FXML private ImageView suspectOneSelected;
+  @FXML private ImageView suspectTwoSelected;
+  @FXML private ImageView suspectThreeSelected;
+  @FXML private ImageView crimeSceneSelected;
+
   @FXML private Label timerLabel;
 
   AppTimer appTimer;
+  ArrayList<ImageView> selectedList = new ArrayList<>();
 
   /** Initializes the game scene and sets the initial game view. */
   @FXML
   public void initialize() {
     System.out.println("Initialising game scene...");
     setGameView(AppUi.SUSPECT1);
+    showSelected(suspectOneSelected);
 
     appTimer = new AppTimer(timerLabel, AppTimer.GAMETIME);
     appTimer.beginCountdown();
+    addAllSelected();
   }
 
   /**
@@ -75,20 +81,38 @@ public class GameController {
   @FXML
   public void handleSuspect1ButtonClick(ActionEvent event) {
     setGameView(AppUi.SUSPECT1);
+    showSelected(suspectOneSelected);
   }
 
   @FXML
   public void handleSuspect2ButtonClick(ActionEvent event) {
     setGameView(AppUi.SUSPECT2);
+    showSelected(suspectTwoSelected);
   }
 
   @FXML
   public void handleSuspect3ButtonClick(ActionEvent event) {
     setGameView(AppUi.SUSPECT3);
+    showSelected(suspectThreeSelected);
   }
 
   @FXML
   public void handleCrimeSceneButtonClick(ActionEvent event) {
     setGameView(AppUi.CRIME_SCENE);
+    showSelected(crimeSceneSelected);
+  }
+
+  public void showSelected(ImageView image) {
+    for (ImageView imageView : selectedList) {
+      imageView.setVisible(false);
+    }
+    image.setVisible(true);
+  }
+
+  public void addAllSelected() {
+    selectedList.add(crimeSceneSelected);
+    selectedList.add(suspectOneSelected);
+    selectedList.add(suspectTwoSelected);
+    selectedList.add(suspectThreeSelected);
   }
 }
