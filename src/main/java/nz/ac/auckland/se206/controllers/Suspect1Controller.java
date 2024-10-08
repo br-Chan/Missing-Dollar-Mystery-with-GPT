e0948610c7efac9ba563256e1152277ced14f242
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-10-08 13:10:10 +1300

    feat: tweak Louie's prompt (about leaving after 11) and edit temperatures & topP for Louie and card clue chat.

--- src/main/java/nz/ac/auckland/se206/controllers/Suspect1Controller.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Suspect1Controller.java
@@ -8,8 +8,8 @@ public class Suspect1Controller extends AbstractSuspectController {
     suspectName = "Louie";
     promptFilename = "suspect1.txt";
 
-    // temperature = 0.9;
-    // topP = 0.9;
+    temperature = 0.6;
+    topP = 0.6;
 
     /*
      * Uncomment the following if you want to disable this suspect's normal prompt and make them a
